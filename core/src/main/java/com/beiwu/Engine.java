fc5bd44055275c7108296d5fc2e14544c98197c3
zhoubing <1050015785@qq.com>
2021-06-22 23:38:26 +0800

    自å®自己实现datax

---
+++ core/src/main/java/com/beiwu/Engine.java
@@ -0,0 +1,71 @@
+package com.beiwu;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.beiwu.exception.CusException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author zhoubing
+ * @date 2021-06-22 22:20
+ */
+public class Engine {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Engine.class);
+
+    public static void main(String[] args) {
+        System.out.println("com.beiwu.Engine begin...");
+
+        try {
+            entry(args);
+        } catch (ParseException e) {
+            throw new CusException("parse error" + e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void entry(String[] args) throws ParseException, IOException {
+
+        Options options = new Options();
+        options.addOption("config", true, "config path");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        String path = cmd.getOptionValue("config");
+
+        LOG.debug("got config path : {}", path);
+
+
+        Map configMap = readConfig(path);
+
+
+
+    }
+
+    private static Map readConfig(String path) throws IOException {
+
+        List<String> strings = Files.readAllLines(new File(path).toPath());
+        StringBuilder sb = new StringBuilder();
+        for (String string : strings) {
+            sb.append(string).append("\n");
+        }
+
+        Map jsonObject = JSON.parseObject(sb.toString());
+        LOG.debug(((JSONObject) jsonObject).toJSONString());
+
+        return jsonObject;
+    }
+}
